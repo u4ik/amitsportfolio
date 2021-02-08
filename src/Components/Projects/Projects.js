92cdb50d1667ec44a5583ece6c377646d09d4bab
u4ik <affirmativeamit@gmail.com>
2021-02-07 22:26:06 -0500

    about

--- src/Components/Projects/Projects.js
+++ src/Components/Projects/Projects.js
@@ -13,6 +13,7 @@ const Projects = () => {
 
 
             <h1 style={{ textShadow: '2px 2px 1px black', marginTop: '2%' }}>Projects</h1>
+            <p>Newest to oldest.</p>
             {/* <MDBAnimation type='fadeInUp' duration='1s' style={{ pointerEvents: 'none !important' }}> */}
             <div style={{ display: 'flex', flexWrap: 'wrap', flexDirection: 'row', justifyContent: 'center', marginBottom: '2%', marginTop: '2%' }}>
 
@@ -22,17 +23,17 @@ const Projects = () => {
                 <div >
                     <div className="projectWrap" style={{ zIndex: 5, opacity: 1 }}>
 
-                            <div className="projectImageContent">
-                     
-                                <a href="https://www.fishnetworld.com">
-                                    <img className="projectImage" src={FishNETThumbnail} id="" alt="" />
-                                    <div className="overlay">
-                                        <h1 className='projectTitle'>FishNET</h1>
-                                        <p className="projectAbout">Commercial social media web application for aquarists. </p>
-                                    </div>
-                                </a>
-                    
-                            </div>
+                        <div className="projectImageContent">
+
+                            <a href="https://www.fishnetworld.com">
+                                <img className="projectImage" src={FishNETThumbnail} id="" alt="" />
+                                <div className="overlay">
+                                    <h1 className='projectTitle'>FishNET</h1>
+                                    <p className="projectAbout">Commercial social media web application for aquarists. </p>
+                                </div>
+                            </a>
+
+                        </div>
 
                     </div>
 
@@ -43,6 +44,71 @@ const Projects = () => {
 
                 </div>
 
+                {/* Premier */}
+                <div>
+                    <div className="projectWrap" style={{ zIndex: 5, opacity: 1 }}>
+                        <div className="projectImageContent">
+                            <a href="https://premier-client.herokuapp.com/">
+                                <img className="projectImage" src={PremierThumbnail} id="" alt="" />
+                                <div className="overlay">
+                                    <h1 class='projectTitle'>Premier Commercial Services</h1>
+                                    <p class="projectAbout">PERN web app. Client/Server for a cleaning company. User Signup-Login/Admin Account/Database Encryption/Testimonials/Orders/. </p>
+                                </div>
+                            </a>
+                        </div>
+
+                    </div>
+
+                    <h3 className='figure-caption' style={{ color: 'white', fontSize: '1.2em', opacity: '.8', textShadow: '1px 1px 1px black' }}>Premier Commercial Services</h3>
+                    <figcaption className="figure-caption text-center">
+                        PERN web app, for a cleaning company.
+                    </figcaption>
+
+                </div>
+
+                {/* Pokemon API */}
+                <div>
+                    <div className="projectWrap" style={{ zIndex: 5, opacity: 1 }}>
+                        <div className="projectImageContent">
+                            <a href="https://u4ik.github.io/PokemonAPIReact/">
+                                <img className="projectImage" src={PokemonThumbnail} id="" alt="" />
+                                <div className="overlay">
+                                    <h1 class='projectTitle'>Pokemon</h1>
+                                    <p class="projectAbout">Pokemon API web app built with React.</p>
+                                </div>
+                            </a>
+                        </div>
+
+                    </div>
+
+                    <h3 className='figure-caption' style={{ color: 'white', fontSize: '1.2em', opacity: '.8', textShadow: '1px 1px 1px black' }}>Pokemon API</h3>
+                    <figcaption className="figure-caption text-center">
+                        Pokemon API web app built with React.
+                    </figcaption>
+
+                </div>
+
+        {/* Jotter */}
+        <div>
+                    <div className="projectWrap" style={{ zIndex: 5, opacity: 1 }}>
+                        <div className="projectImageContent">
+                            <a href="https://jotter-app-client.herokuapp.com/">
+                                <img className="projectImage" src={JotterThumbnail} id="" alt="" />
+                                <div className="overlay">
+                                    <h1 class='projectTitle'>Jotter</h1>
+                                    <p class="projectAbout">Personal notes client/server PERN web app.</p>
+                                </div>
+                            </a>
+                        </div>
+
+                    </div>
+
+                    <h3 className='figure-caption' style={{ color: 'white', fontSize: '1.2em', opacity: '.8', textShadow: '1px 1px 1px black' }}>Jotter</h3>
+                    <figcaption className="figure-caption text-center">
+                        Personal notes client/server PERN web app.
+                    </figcaption>
+
+                </div>
 
 
 
@@ -92,71 +158,10 @@ const Projects = () => {
 
                 </div>
 
-                {/* Pokemon API */}
-                <div>
-                    <div className="projectWrap" style={{ zIndex: 5, opacity: 1 }}>
-                        <div className="projectImageContent">
-                            <a href="https://u4ik.github.io/PokemonAPIReact/">
-                                <img className="projectImage" src={PokemonThumbnail} id="" alt="" />
-                                <div className="overlay">
-                                    <h1 class='projectTitle'>Pokemon</h1>
-                                    <p class="projectAbout">Pokemon API web app built with React.</p>
-                                </div>
-                            </a>
-                        </div>
 
-                    </div>
 
-                    <h3 className='figure-caption' style={{ color: 'white', fontSize: '1.2em', opacity: '.8', textShadow: '1px 1px 1px black' }}>Pokemon API</h3>
-                    <figcaption className="figure-caption text-center">
-                        Pokemon API web app built with React.
-                    </figcaption>
+        
 
-                </div>
-
-                {/* Jotter */}
-                <div>
-                    <div className="projectWrap" style={{ zIndex: 5, opacity: 1 }}>
-                        <div className="projectImageContent">
-                            <a href="https://jotter-app-client.herokuapp.com/">
-                                <img className="projectImage" src={JotterThumbnail} id="" alt="" />
-                                <div className="overlay">
-                                    <h1 class='projectTitle'>Jotter</h1>
-                                    <p class="projectAbout">Personal notes client/server PERN web app.</p>
-                                </div>
-                            </a>
-                        </div>
-
-                    </div>
-
-                    <h3 className='figure-caption' style={{ color: 'white', fontSize: '1.2em', opacity: '.8', textShadow: '1px 1px 1px black' }}>Jotter</h3>
-                    <figcaption className="figure-caption text-center">
-                        Personal notes client/server PERN web app.
-                    </figcaption>
-
-                </div>
-
-                {/* Premier */}
-                <div>
-                    <div className="projectWrap" style={{ zIndex: 5, opacity: 1 }}>
-                        <div className="projectImageContent">
-                            <a href="https://premier-client.herokuapp.com/">
-                                <img className="projectImage" src={PremierThumbnail} id="" alt="" />
-                                <div className="overlay">
-                                    <h1 class='projectTitle'>Premier Commercial Services</h1>
-                                    <p class="projectAbout">PERN web app. Client/Server for a cleaning company. User Signup-Login/Admin Account/Database Encryption/Testimonials/Orders/. </p>
-                                </div>
-                            </a>
-                        </div>
-
-                    </div>
-
-                    <h3 className='figure-caption' style={{ color: 'white', fontSize: '1.2em', opacity: '.8', textShadow: '1px 1px 1px black' }}>Premier Commercial Services</h3>
-                    <figcaption className="figure-caption text-center">
-                        PERN web app, for a cleaning company.
-                    </figcaption>
-
-                </div>
 
 
 
