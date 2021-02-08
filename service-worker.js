184bb94504f24f35f4a0fb03fe06757f0a11051e
u4ik <affirmativeamit@gmail.com>
2021-02-07 22:19:24 -0500

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.e64fa827b119ca62fc4677a538b964ac.js"
+  "/precache-manifest.c3e9863714f7f84a1a09e53384fd0b49.js"
 );
 
 self.addEventListener('message', (event) => {
